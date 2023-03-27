67e952ef69f3c128c30ed6e36e6ed84d2071b092
VVCephei <124810683+Nekrofurus@users.noreply.github.com>
2023-03-27 10:14:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,25 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
-Console.WriteLine("Для того, чтобы вывести на экран значение функции Аккермана(A(n,m))");
-int numberN = UserInput("Введите n: ");
+Console.WriteLine("Для того, чтобы вывести на экран значение функции Аккермана(A(m,n))");
 int numberM = UserInput("Введите m: ");
-
+int numberN = UserInput("Введите n: ");
 if (NotNegative(numberN, numberM))
 {
     Console.WriteLine("Для вычисления функции Аккермана значения должны быть не отрицательными целыми числами");
 }
 else
 {
-    Console.WriteLine($"Значение функции A({numberN},{numberM}) = {Ack(numberN, numberM)}");
+    Console.WriteLine($"Значение функции A({numberM},{numberN}) = {Ack(numberM, numberN)}");
 }
-int Ack(int numN, int numM)
+int Ack(int numM, int numN)
 {
-    if (numN == 0)
-        return numM + 1;
-    else if (numM == 0)
-        return checked(Ack(numN - 1, 1));
+    if (numM == 0)
+        return numN + 1;
+    else if (numN == 0)
+        return checked(Ack(numM - 1, 1));
     else
-        return checked(Ack(numN - 1, Ack(numN, numM - 1)));
+        return checked(Ack(numM - 1, Ack(numM, numN - 1)));
 }
 bool NotNegative(int numN, int numM)
 {
